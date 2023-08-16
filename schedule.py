347963574655b99c73eed5b502f79e7e44217c62
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-08-16 13:04:10 +0300

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -23,33 +23,41 @@ search_name = st.sidebar.text_input("Search by Client Name", "")
 def format_number(number):
     return '{:,.0f}'.format(number)
 
+
 # Filtering based on user input
 if search_policy:
     policy_results = df[df['Policy Number'].str.contains(search_policy, case=False)]
 
-    # Replace NaN values in 'Installment' with corresponding 'Claim Amount'
-    policy_results['Installment'] = policy_results['Installment'].fillna(policy_results['Claim Amount'])
-    
-    # Format 'Date Scheduled' column to display full month name
-    policy_results['Date Scheduled'] = pd.to_datetime(policy_results['Date Scheduled']).dt.strftime('%B %d, %Y')
+    if policy_results.empty:
+        st.write("Data Not Available")
+    else:
+        # Replace NaN values in 'Installment' with corresponding 'Claim Amount'
+        policy_results['Installment'] = policy_results['Installment'].fillna(policy_results['Claim Amount'])
 
-    # Format 'Claim Amount' and 'Installment' columns with commas and no decimal places
-    policy_results['Claim Amount'] = policy_results['Claim Amount'].apply(format_number)
-    policy_results['Installment'] = policy_results['Installment'].apply(format_number)
+        # Format 'Claim Amount' and 'Installment' columns with commas and no decimal places
+        policy_results['Claim Amount'] = policy_results['Claim Amount'].apply(format_number)
+        policy_results['Installment'] = policy_results['Installment'].apply(format_number)
 
+        # Format 'Date Scheduled' column to display full month name
+        policy_results['Date Scheduled'] = pd.to_datetime(policy_results['Date Scheduled']).dt.strftime('%B %d, %Y')
 
-    st.table(policy_results[['Insured ', 'Policy Number', 'Claim Type', 'Date Scheduled', 'Claim Amount', 'Installment']])
+        st.table(policy_results[['Insured ', 'Policy Number', 'Claim Type', 'Date Scheduled', 'Claim Amount', 'Installment']])
 
 if search_name:
     name_results = df[df['Insured '].str.contains(search_name, case=False)]
 
-    # Replace NaN values in 'Installment' with corresponding 'Claim Amount'
-    name_results['Installment'] = name_results['Installment'].fillna(name_results['Claim Amount'])
-    # Format 'Date Scheduled' column to display full month name
-    name_results['Date Scheduled'] = pd.to_datetime(name_results['Date Scheduled']).dt.strftime('%B %d, %Y')
+    if name_results.empty:
+        st.write("Data Not Available")
+    else:
+        # Replace NaN values in 'Installment' with corresponding 'Claim Amount'
+        name_results['Installment'] = name_results['Installment'].fillna(name_results['Claim Amount'])
 
-    # Format 'Claim Amount' and 'Installment' columns with commas and no decimal places
-    name_results['Claim Amount'] = name_results['Claim Amount'].apply(format_number)
-    name_results['Installment'] = name_results['Installment'].apply(format_number)
-    
-    st.table(name_results[['Insured ', 'Policy Number', 'Claim Type', 'Date Scheduled', 'Claim Amount', 'Installment']])
+        # Format 'Claim Amount' and 'Installment' columns with commas and no decimal places
+        name_results['Claim Amount'] = name_results['Claim Amount'].apply(format_number)
+        name_results['Installment'] = name_results['Installment'].apply(format_number)
+
+        # Format 'Date Scheduled' column to display full month name
+        name_results['Date Scheduled'] = pd.to_datetime(name_results['Date Scheduled']).dt.strftime('%B %d, %Y')
+
+        st.table(name_results[['Insured ', 'Policy Number', 'Claim Type','Date Scheduled', 'Claim Amount', 'Installment']])
+
