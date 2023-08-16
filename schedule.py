f3e436c13e27e6d1f7e9e4db0d057f60a94e1de6
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-08-16 12:19:03 +0300

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -1,2 +1,12 @@
 import streamlit as st
 import pandas as pd
+
+# configuration
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+# title of the app
+st.title("Life Claims Schedule ")
+
+# Add a sidebar
+st.sidebar.image('corplogo.PNG', use_column_width=True)
+st.sidebar.subheader("Search Clients Details")
