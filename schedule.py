599ade2fcc59760f2ee48c436f242b70ead96138
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-08-16 12:28:01 +0300

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -10,3 +10,22 @@ st.title("Life Claims Schedule ")
 # Add a sidebar
 st.sidebar.image('corplogo.PNG', use_column_width=True)
 st.sidebar.subheader("Search Clients Details")
+
+# Load the CSV file
+csv_file_path = 'finalclientdata.csv'  
+df = pd.read_csv(csv_file_path)
+
+# Sidebar input boxes
+search_policy = st.sidebar.text_input("Search by Policy Number", "")
+search_name = st.sidebar.text_input("Search by Client Name", "")
+
+# Filtering based on user input
+if search_policy:
+    policy_results = df[df['Policy Number'].str.contains(search_policy, case=False)]
+    
+    st.write(policy_results)
+
+if search_name:
+    name_results = df[df['Insured'].str.contains(search_name, case=False)]
+    
+    st.write(name_results)
