b0d30fd93c2627fe51d56c267a50b2845fde8486
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-08-16 12:53:37 +0300

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -22,10 +22,22 @@ search_name = st.sidebar.text_input("Search by Client Name", "")
 # Filtering based on user input
 if search_policy:
     policy_results = df[df['Policy Number'].str.contains(search_policy, case=False)]
+
+    # Replace NaN values in 'Installment' with corresponding 'Claim Amount'
+    policy_results['Installment'] = policy_results['Installment'].fillna(policy_results['Claim Amount'])
     
+    # Format 'Date Scheduled' column to display full month name
+    policy_results['Date Scheduled'] = pd.to_datetime(policy_results['Date Scheduled']).dt.strftime('%B %d, %Y')
+
     st.table(policy_results[['Insured ', 'Policy Number', 'Claim Type', 'Maturity Year', 'Date Scheduled', 'Claim Amount', 'Installment']])
 
 if search_name:
     name_results = df[df['Insured '].str.contains(search_name, case=False)]
+
+    # Replace NaN values in 'Installment' with corresponding 'Claim Amount'
+    name_results['Installment'] = name_results['Installment'].fillna(name_results['Claim Amount'])
+    # Format 'Date Scheduled' column to display full month name
+    name_results['Date Scheduled'] = pd.to_datetime(name_results['Date Scheduled']).dt.strftime('%B %d, %Y')
+
     
     st.table(name_results[['Insured ', 'Policy Number', 'Claim Type', 'Maturity Year', 'Date Scheduled', 'Claim Amount', 'Installment']])
