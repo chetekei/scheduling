350dc36be10bc198efc6d276a8f3d8f98fca59a6
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-08-17 08:43:11 +0300

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -68,7 +68,7 @@ if search_name:
        # Style the table
         styled_results = name_results[['Insured ', 'Policy Number', 'Claim Type', 'Date Scheduled', 'Claim Amount', 'Installment']].style\
             .set_table_styles([{'selector': 'th',
-                                'props': [('background-color', '#FF55A3'),
+                                'props': [('background-color', '#ec3b83'),
                                           ('font-weight', 'bold')]}])
 
         st.table(styled_results)
