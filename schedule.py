f0e04525c92b79f5e7596a9e7010d4e859b4bddd
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-08-28 09:51:15 +0300

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -102,7 +102,7 @@ if st.sidebar.button("Download Scheduled Policies for This Week"):
     columns_to_include = ['Claim Type', 'Insured ', 'Policy Number', 'Sum Assured', 'Claim Amount', 'Date Scheduled']
     policies_selected_columns = policies_this_week[columns_to_include]
 
-    st.subheader("Policies Scheduled for This Week")
+    st.subheader("Payments Scheduled for This Week")
     st.dataframe(policies_selected_columns)
     
     # Provide the download link in the sidebar
