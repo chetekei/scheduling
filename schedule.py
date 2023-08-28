f4c94f65d677fed5746f067e5bbd1f43e3aff715
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-08-28 09:17:41 +0300

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -92,8 +92,5 @@ if st.sidebar.button("Download Scheduled Policies for This Week"):
     # Filter the DataFrame to get policies scheduled for this week
     policies_this_week = df[(pd.to_datetime(df['Date Scheduled']) >= start_of_week) & (pd.to_datetime(df['Date Scheduled']) <= end_of_week)]
     
-    # Save the filtered DataFrame to a CSV file
-    policies_this_week.to_csv('policies_this_week.csv', index=False)
-    
     # Provide the download link in the sidebar
-    st.sidebar.markdown("[Download Scheduled Policies for This Week](policies_this_week.csv)")
+    st.sidebar.markdown(get_download_link(policies_this_week), unsafe_allow_html=True)
