d68a317e828caf3ce1dde58888df503ed20dfbf6
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-08-30 10:16:05 +0300

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -45,9 +45,9 @@ if search_policy:
 
         # Format 'Date Scheduled' column to display full month name
         policy_results['Date Scheduled'] = pd.to_datetime(policy_results['Date Scheduled']).dt.strftime('%B %d, %Y')
-        #policy_results['Re-scheduled Date'] = pd.to_datetime(policy_results['Re-scheduled Date']).dt.strftime('%B %d, %Y')
+        policy_results['Re - scheduled Date'] = pd.to_datetime(policy_results['Re - scheduled Date']).dt.strftime('%B %d, %Y')
 
-        styled_results = policy_results[['Insured ', 'Policy Number', 'Claim Type', 'Date Scheduled', 'Claim Amount', 'Installment']].style\
+        styled_results = policy_results[['Insured ', 'Policy Number', 'Claim Type', 'Date Scheduled', 'Re - scheduled Date', 'Claim Amount', 'Installment']].style\
             .set_table_styles([{'selector': 'th',
                                 'props': [('background-color', '#f19cbb'),
                                           ('font-weight', 'bold')]}])
