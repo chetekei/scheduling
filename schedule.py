36ec67ec74875daefb8d9713eeffc68ed73c01f4
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-08-30 10:17:41 +0300

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -69,12 +69,13 @@ if search_name:
 
         # Format 'Date Scheduled' column to display full month name
         name_results['Date Scheduled'] = pd.to_datetime(name_results['Date Scheduled']).dt.strftime('%B %d, %Y')
+        name_results['Re - scheduled Date'] = pd.to_datetime(name_results['Re - scheduled Date']).dt.strftime('%B %d, %Y')
 
          # Reset the index and remove the default index column
         name_results.reset_index(drop=True, inplace=True)
 
        # Style the table
-        styled_results = name_results[['Insured ', 'Policy Number', 'Claim Type', 'Date Scheduled', 'Claim Amount', 'Installment']].style\
+        styled_results = name_results[['Insured ', 'Policy Number', 'Claim Type', 'Date Scheduled', 'Re - scheduled Date', 'Claim Amount', 'Installment']].style\
             .set_table_styles([{'selector': 'th',
                                 'props': [('background-color', '#f19cbb'),
                                           ('font-weight', 'bold')]}])
