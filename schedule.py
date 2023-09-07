429d4c5b4dad3482bb94b0489c82a4b64397b049
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-09-07 10:16:44 +0300

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -44,10 +44,10 @@ if search_policy:
         policy_results['Installment'] = policy_results['Installment'].apply(format_number)
 
         # Format 'Date Scheduled' column to display full month name
-        policy_results['Date Scheduled'] = pd.to_datetime(policy_results['Date Scheduled'])
-        policy_results['Re - scheduled Date'] = pd.to_datetime(policy_results['Re - scheduled Date'])
+        policy_results[' Date Scheduled'] = pd.to_datetime(policy_results[' Date Scheduled']).dt.strftime('%B %d, %Y')
+        policy_results['Re - scheduled Date'] = pd.to_datetime(policy_results['Re - scheduled Date']).dt.strftime('%B %d, %Y')
 
-        styled_results = policy_results[['Insured ', 'Policy Number', 'Claim Type', 'Date Scheduled', 'Re - scheduled Date', 'Claim Amount', 'Installment']].style\
+        styled_results = policy_results[['Insured ', 'Policy Number', 'Claim Type', ' Date Scheduled', 'Re - scheduled Date', 'Claim Amount', 'Installment']].style\
             .set_table_styles([{'selector': 'th',
                                 'props': [('background-color', '#f19cbb'),
                                           ('font-weight', 'bold')]}])
@@ -66,14 +66,14 @@ if search_name:
         name_results['Installment'] = name_results['Installment'].apply(format_number)
 
         
-        name_results['Date Scheduled'] = pd.to_datetime(name_results['Date Scheduled'])
-        name_results['Re - scheduled Date'] = pd.to_datetime(name_results['Re - scheduled Date'])
+        name_results[' Date Scheduled'] = pd.to_datetime(name_results[' Date Scheduled']).dt.strftime('%B %d, %Y')
+        name_results['Re - scheduled Date'] = pd.to_datetime(name_results['Re - scheduled Date']).dt.strftime('%B %d, %Y')
 
          # Reset the index and remove the default index column
         name_results.reset_index(drop=True, inplace=True)
 
        # Style the table
-        styled_results = name_results[['Insured ', 'Policy Number', 'Claim Type', 'Date Scheduled', 'Re - scheduled Date', 'Claim Amount', 'Installment']].style\
+        styled_results = name_results[['Insured ', 'Policy Number', 'Claim Type', ' Date Scheduled', 'Re - scheduled Date', 'Claim Amount', 'Installment']].style\
             .set_table_styles([{'selector': 'th',
                                 'props': [('background-color', '#f19cbb'),
                                           ('font-weight', 'bold')]}])
